c72997ca7a3b95b6a8943cf302c5fd71a5d0a9a8
Evgeniy Budaev <budaev.e@gmail.com>
2026-01-03 08:48:39 +0300

    feat: liquibase

---
+++ modules/aggregation-jpa/src/main/java/com/aggregationjpa/AggregationJpaApplication.java
@@ -0,0 +1,13 @@
+package com.aggregationjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggregationJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AggregationJpaApplication.class, args);
+    }
+
+}
